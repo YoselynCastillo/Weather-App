78bdcd1b560f49415211d882c812ea374cecf164
YoselynCastillo <yose.castillo06@gmail.com>
2020-06-01 14:06:30 -0400

    Añadida la propiedad de localStorage

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,12 @@
 //se supone que con esto, yo importo la clase Weather del documento Weather.js
 const { Weather } = require("./Weather");
 const { UI } = require("./UI");
+const { Store } = require("./Store");
 
 //nuevo objeto de la clase Weather
-
-const weather = new Weather('Kiev', 'ua');
+const store = new Store();
+const { city, countryCode } = store.getLocationData();
+const weather = new Weather(city, countryCode);
 const ui = new UI();
 
 require("./index.css");
@@ -19,6 +21,7 @@ document.getElementById("w-change-btn").addEventListener("click", (e) => {
   const city = document.getElementById("city").value;
   const countryCode = document.getElementById("countryCode").value;
   weather.changeLocation(city, countryCode);
+  store.setLocationData(city, countryCode);
   fetchWeather();
   e.preventDefault();
 });
